4f674d4096ef3a90d682de7a19cd019496311008
VvePrime <vive021172@gmail.com>
2024-02-29 20:30:05 +0530

    initial commit

---
+++ src/test/java/com/vivek/batvball/BatvballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivek.batvball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatvballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
