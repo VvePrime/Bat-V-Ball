5d7edcbae11111ec449bc58e1660bb9d0cb5719a
VvePrime <vive021172@gmail.com>
2024-03-25 13:39:49 +0530

    multi env - multi properties files

--- src/main/java/com/vivek/batvball/BatvballApplication.java
+++ src/main/java/com/vivek/batvball/BatvballApplication.java
@@ -2,17 +2,14 @@ package com.vivek.batvball;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("application-{spring.profiles.active}.properties")
 @SpringBootApplication
 public class BatvballApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BatvballApplication.class, args);
-		fetch();
-	}
-	
-	public static void fetch() {
-		
 	}
 
 }
