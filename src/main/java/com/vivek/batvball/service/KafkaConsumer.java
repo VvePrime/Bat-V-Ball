7ee102a20448b1d38f236a75260f35cc90525ab4
VvePrime <vive021172@gmail.com>
2024-06-09 19:48:32 +0530

    port automatic and instance id added for eureka server

--- src/main/java/com/vivek/batvball/service/KafkaConsumer.java
+++ src/main/java/com/vivek/batvball/service/KafkaConsumer.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,8 @@ public class KafkaConsumer {
 	@Autowired
 	PlayerService playerService;
 	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
     @KafkaListener(topics = "bat-v-ball", groupId = "umpires")
     public void listen(ConsumerRecord<String, String> record) {
     	ScoreCardInputDTO dto = new ScoreCardInputDTO();
@@ -23,6 +27,6 @@ public class KafkaConsumer {
     	dto.setScoreCard(record.value());
     	dto.setDateString(new SimpleDateFormat("dd-MM-yyyy").format(new Date()));
     	playerService.processInput(dto);
-        System.out.println( record.value() +" is todays Score Card of the Batsman with id : " + record.key() +"date: "+ dto.getDateString());
+       logger.info( record.value() +" is todays Score Card of the Batsman with id : " + record.key() +"date: "+ dto.getDateString());
     }
 }
